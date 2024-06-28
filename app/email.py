b8d086e28f0bd7c508fc68e7a907dcb3697c0468
Matthew Frost <m.frost@mattronix.nl>
2024-06-28 18:32:55 +0200

    default EMAIL template

--- app/email.py
+++ app/email.py
@@ -6,7 +6,7 @@ from flask import render_template
 
 from app.settings import MAIL_DEFAULT_SENDER
 
-def send_html_email(subject, recipients, template, sender=MAIL_DEFAULT_SENDER):
+def send_html_email(subject, recipients, template="email_default_template.html", sender=MAIL_DEFAULT_SENDER):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.html = render_template(template)
     mail.send(msg)
